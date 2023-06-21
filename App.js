d4219f82b800d53d639f28274ff82510c585e9f2
Noemi Sarid Azocar Ramirez <115116514+nazocar@users.noreply.github.com>
2023-06-21 01:36:03 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.mapStyle}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker 
+          coordinate={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+          }}
+        />
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: '100%'
+  }
+});
